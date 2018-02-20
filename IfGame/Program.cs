d958a81f64bda47ebf5afd923d6812c4e5f35751
johnnybrunson <36446410+johnnybrunson@users.noreply.github.com>
2018-02-20 09:46:15 -0500

    Added code

--- IfGame/Program.cs
+++ IfGame/Program.cs
@@ -6,7 +6,24 @@ namespace IfGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Let's play a game. You must select door 1, 2 or 3 to see what you have won.");
+            string UserValue = Console.ReadLine();
+
+            string message = "";
+
+            if (UserValue == "1")
+                message = "You won a new MacBookPro!";
+            else if (UserValue == "2")
+                message = "You won a new SurfacePro!";
+            else if (UserValue == "3")
+                message = "You won a 1 year MSDN subscription!";
+            else
+            {
+                message = "Since you can't follow instructions, you win nothing.";
+            }
+
+            Console.WriteLine(message);
+            Console.ReadLine();
         }
     }
 }
